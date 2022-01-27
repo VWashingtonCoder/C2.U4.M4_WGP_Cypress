93d4c9c39e2668f2a3dd029901f8cfbfcd4b4d06
Casey Harding <casey.r.harding@gmail.com>
2022-01-27 12:37:53 -0500

    Start of sanity check.

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,22 @@
 // write tests here
+describe("Quotes App", () => {
+  beforeEach(() => {
+    // Each test needs fresh state!
+    // Tests should NEVER rely on state left over from previous tests
+    // Every test should able to work in isolation
+    cy.visit("http://localhost:1234");
+  })
+
+  // Centralize the getters
+  const textInput = () => cy.get('input[name=text]');
+  const authorInput = () => cy.get('input[name=author]');
+  const foobarInput = () => cy.get('input[name=foobar]');
+  const submitBtn = () => cy.get(`button[id="submitBtn"]`);
+  const cancelBtn = () => cy.get(`button[id="cancelBtn"]`);
+
+  it("sanity check to make sure that tests work", () => {
+    // "it" is a test
+    // "expect is an assertion"
+    expect(1 + 2).to.equal(3);
+  })
+})
